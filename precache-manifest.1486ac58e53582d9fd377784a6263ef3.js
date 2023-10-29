8b7f27c14615734ca2f7db531b7147d4e5c1f40d
Teeboi5228 <oluwaleyetaiwo8225@gmail.com>
2023-10-29 06:50:14 +0100

    Updates

---
+++ precache-manifest.1486ac58e53582d9fd377784a6263ef3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0afc5b9cdd4f427cf411db66c13cb71e",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "b5c333ea362cff93ad0b",
+    "url": "/react/static/css/main.557c0ea4.chunk.css"
+  },
+  {
+    "revision": "99b1961c55d675e491f5",
+    "url": "/react/static/js/2.8808d590.chunk.js"
+  },
+  {
+    "revision": "089bc876b02613239fa5",
+    "url": "/react/static/js/3.b7c4cbb6.chunk.js"
+  },
+  {
+    "revision": "b5c333ea362cff93ad0b",
+    "url": "/react/static/js/main.be62464a.chunk.js"
+  },
+  {
+    "revision": "b29675fd16217445153e",
+    "url": "/react/static/js/runtime~main.0575fdc1.js"
+  },
+  {
+    "revision": "92201d81697cee036eb5d5b78cf97729",
+    "url": "/react/static/media/beach-work.92201d81.jpg"
+  },
+  {
+    "revision": "9da6997f45c2702ab612cce8af0efec8",
+    "url": "/react/static/media/chicago.9da6997f.jpg"
+  },
+  {
+    "revision": "884a7f6cf3b58983287376276dac78d9",
+    "url": "/react/static/media/colorado.884a7f6c.jpg"
+  },
+  {
+    "revision": "376aa91cae7919755ce7cc0721cfe612",
+    "url": "/react/static/media/malibu.376aa91c.jpg"
+  },
+  {
+    "revision": "7290d3e8e93d121fec0481d8d9cb20e3",
+    "url": "/react/static/media/miami.7290d3e8.jpg"
+  },
+  {
+    "revision": "efbb92570004cc17c1f330510268694d",
+    "url": "/react/static/media/seattle.efbb9257.jpg"
+  },
+  {
+    "revision": "887989d0b312b08a867bea3909888f8e",
+    "url": "/react/static/media/toronto.887989d0.jpg"
+  }
+]);
